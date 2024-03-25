3ec4ee9f0e5315629e648e6abf39b7e094244a27
Galakitkkon <malekmarek56@gmail.com>
2024-03-25 23:11:53 +0100

    registration and login works

---
+++ backend/backend/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from rest_framework import generics
+from .serializers import RegisterSerializer, GroupSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
