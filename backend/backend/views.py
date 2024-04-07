bc0d804022564fee188102a2e78b8177d6eb1eeb
Galakitkkon <malekmarek56@gmail.com>
2024-04-07 21:46:42 +0200

    basic interval endpoint, unittest - need to consult!

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
 from rest_framework import generics
-from .serializers import RegisterSerializer, GroupSerializer, UserSerializer
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer, GroupSerializer, UserSerializer, IntervalSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from model.interval_generator import IntervalGenerator
 from rest_framework.views import APIView
 from .serializers import RegisterSerializer, GroupSerializer, UserSerializer, ChordSerializer
 from rest_framework.response import Response
@@ -41,6 +45,20 @@ class ChordView(APIView):
 
             generator = ChordGenerator(serializer_class.data)
 
+            return Response(generator.draw(), status=status.HTTP_200_OK)
+
+        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class IntervalView(APIView):
+
+    def post(self, request, format=None):
+        serializer_class = IntervalSerializer(data=request.data)
+        if serializer_class.is_valid():
+
+            generator = IntervalGenerator(serializer_class.data)
+
+            # first, second then another request or first, second, answear and logic at FE?
             return Response(generator.draw(), status=status.HTTP_200_OK)
 
         return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
