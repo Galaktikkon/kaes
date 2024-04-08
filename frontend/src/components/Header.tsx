25b7d16f903307534fe126ce15db923d93d9d731
Galakitkkon <malekmarek56@gmail.com>
2024-04-08 13:12:20 +0200

    added Header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import {
+  Box,
+  Flex,
+  Avatar,
+  Text,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  VStack,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const NavLink = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={"#"}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const session = useSession();
+
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Box>MusicMaster</Box>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar size={"sm"} />
+                </MenuButton>
+                <MenuList alignItems={"center"}>
+                  <br />
+                  <Center>
+                    <Avatar size={"2xl"} />
+                  </Center>
+                  <br />
+                  <Center>
+                    {session.data?.expires ? (
+                      <VStack>
+                        <p>{session.data?.expires}</p>
+                        <Button onClick={() => signOut()}>Sign out</Button>
+                      </VStack>
+                    ) : (
+                      <Link href={"/signin"}>Not logged in, sign in here</Link>
+                    )}
+                  </Center>
+
+                  <br />
+                  <MenuDivider />
+                  {/* <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem> */}
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
