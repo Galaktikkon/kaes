6e47cdb94e47b6ab37c0d81bda60ec3c3f6c77b0
Galakitkkon <malekmarek56@gmail.com>
2024-05-06 03:38:54 +0200

    added services - fetching and sampler replay

---
+++ frontend/src/services/sampler.ts
@@ -0,0 +1,15 @@
+import { Sampler } from "tone";
+import game from "../state/Game";
+
+const sampler: Sampler = new Sampler({
+  urls: {
+    C4: "C4.mp3",
+    "D#4": "Ds4.mp3",
+    "F#4": "Fs4.mp3",
+    A4: "A4.mp3",
+  },
+  release: game.settings.playSettings.noteDuration,
+  baseUrl: "https://tonejs.github.io/audio/salamander/",
+}).toDestination();
+
+export default sampler;
