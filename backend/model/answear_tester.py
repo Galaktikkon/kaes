c311d9dc9753a9aeb2837e4f7bc4099b6d97dab5
Galakitkkon <malekmarek56@gmail.com>
2024-05-20 09:32:36 +0200

    changed answear_tester's variable names

--- backend/model/answear_tester.py
+++ backend/model/answear_tester.py
@@ -5,13 +5,13 @@ from model.utils.semitones import Semitones
 class AnswearTester:
 
     def __init__(self, data):
-        self.group_type = data["group_type"]
+        self.exercise_type = data["exercise_type"]
         self.pitch_sequence = data["pitch_sequence"]
         self.answear_to_check = data["answear_to_check"]
 
     def test_answear(self):
         user_sequence = Sequences.get_type_dict(
-            self.group_type
+            self.exercise_type
         )[self.answear_to_check]
         correct_sequence = self.__get_sequence_from_pitches(
             self.pitch_sequence
