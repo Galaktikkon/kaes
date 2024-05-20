a169b3cc94468bd95c87200374df0a6904323c80
Galakitkkon <malekmarek56@gmail.com>
2024-05-20 16:47:15 +0200

    upgrade semitones and sequence_generator

--- backend/model/utils/semitones.py
+++ backend/model/utils/semitones.py
@@ -14,6 +14,34 @@ class Semitones:
         return 12*octave_count+semitones_high-semitones_low
 
     @classmethod
+    def split_pitch_range(cls, pitch_range: str):
+        octave = int(pitch_range[len(pitch_range)-1])
+        pitch = pitch_range[:len(pitch_range)-1]
+        return pitch, octave
+
+    @classmethod
+    def generate_scale(cls, pitch_range_low, pitch_range_high):
+        pitch_low, octave_low = cls.split_pitch_range(pitch_range_low)
+
+        octave = octave_low
+        pitch = pitch_range_low
+        semitone_value = cls.__SEMITONES_TYPES[pitch_low]
+
+        scale = []
+        while pitch != pitch_range_high:
+
+            pitch = cls.__SEMITONES_TYPES.inverse[semitone_value]+str(octave)
+            scale.append(pitch)
+
+            semitone_value += 1
+
+            if semitone_value > 11:
+                semitone_value %= 12
+                octave += 1
+
+        return scale
+
+    @classmethod
     def get_note_names(cls) -> list[str]:
         return list(cls.__SEMITONES_TYPES.keys())
 
